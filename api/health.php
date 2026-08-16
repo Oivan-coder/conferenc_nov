a90aff11cf228b9764dcfeca358df9577bb9dc8f
Oivan-coder <ivangoltsev8@gmail.com>
2026-08-16 15:55:00 +0300

    Log database health error privately

--- api/health.php
+++ api/health.php
@@ -3,6 +3,7 @@ header('Content-Type: application/json; charset=utf-8');
 header('Cache-Control: no-store');
 
 $configPath = '/home/c/cx314477/private/db.php';
+$errorLogPath = '/home/c/cx314477/private/health_error.log';
 
 $diagnostics = [
     'ok' => false,
@@ -21,9 +22,12 @@ try {
     $pdo->query('SELECT 1');
     $diagnostics['ok'] = true;
     $diagnostics['database'] = true;
+    @unlink($errorLogPath);
     http_response_code(200);
 } catch (Throwable $e) {
     $diagnostics['error_type'] = get_class($e);
+    $safeMessage = get_class($e) . ': ' . $e->getMessage() . PHP_EOL;
+    @file_put_contents($errorLogPath, $safeMessage, LOCK_EX);
     http_response_code(500);
 }
 
